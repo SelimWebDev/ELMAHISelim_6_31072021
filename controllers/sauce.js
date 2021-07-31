ea939e786f8afeb34d8327466b301417a08c5ea4
Selim El Mahi <selim.elmahi@gmail.com>
2021-08-01 00:15:41 +0200

    developement de l'architecture, importations des dépendances nécessaire

---
+++ controllers/sauce.js
@@ -0,0 +1,97 @@
+const Sauce = require('../models/sauce');
+
+
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce)
+  const thing = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    likes: [],
+    dislikes: [],
+    userLiked: [],
+    userDisliked: []
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Sauce créée avec succès!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.getOneSauce = (req, res, next) => {
+  Sauce.findOne({
+    _id: req.params.id
+  }).then(
+    (sauce) => {
+      res.status(200).json(sauce);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.modifyThing = (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Thing.updateOne({_id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.deleteThing = (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find().then(
+    (sauces) => {
+      res.status(200).json(sauces);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
---
+++ routes/sauce.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const tkn = require('../middleware/tkn');
+const multer = require('../middleware/multer-config')
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.get('/', tkn, sauceCtrl.getAllSauces);
+router.get('/:id', tkn, sauceCtrl.getOneSauce);
+router.post('/', tkn, multer, sauceCtrl.createSauce);
+//router.put('/:id', auth, sauceCtrl.modifySauce);
+//router.delete('/:id', auth, sauceCtrl.deleteSauce);
+//router.post('/:id/like', auth, sauceCtrl.setLike);
+
+module.exports = router;
